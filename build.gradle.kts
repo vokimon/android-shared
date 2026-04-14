117fb756d365dd7a4961a8a020675c41b5573b0f
David García Garzón <voki@canvoki.net>
2026-04-14 13:28:04 +0200

    🏗️ activate spotless in solo builds

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.spotless)
     id("net.canvoki.android-yaml-strings") version "1.0.0"
 }
 

dd4e783ad5de0eae20038d232f57efd22d70bb29
David García Garzón <voki@canvoki.net>
2026-06-25 00:52:18 +0200

    🎨 spotless

--- src/main/kotlin/net/canvoki/shared/component/AsyncList.kt
+++ src/main/kotlin/net/canvoki/shared/component/AsyncList.kt
@@ -2,9 +2,9 @@ package net.canvoki.shared.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState

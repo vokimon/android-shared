f38a9918bf7f15d87463a301bf76b99066a70c26
David García Garzón <voki@canvoki.net>
2026-06-24 23:38:56 +0200

    🐛 AsyncList: adding bottom padding for compat with fabs

--- src/main/kotlin/net/canvoki/shared/component/AsyncList.kt
+++ src/main/kotlin/net/canvoki/shared/component/AsyncList.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
@@ -103,9 +104,11 @@ fun <T> AsyncList(
             }
         }
         else -> {
+            val fabHeight = 80.dp
             LazyColumn(
                 modifier = Modifier.fillMaxSize(),
                 state = listState,
+                contentPadding = PaddingValues(bottom = fabHeight),
             ) {
                 if (groupBy != null) {
                     val grouped = items.groupBy(groupBy)
